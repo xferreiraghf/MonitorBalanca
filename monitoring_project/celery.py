3f87376ce935f5c7396bc2d77670d3592cbe761b
Gustavo Henrique Ferreira <143434753+xferreiraghf@users.noreply.github.com>
2025-02-03 16:28:24 -0300

    Update files

--- monitoring_project/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seu_projeto.settings')
-
-app = Celery('seu_projeto')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- monitoring_project/settings.py
+++ monitoring_project/settings.py
@@ -1,13 +1,9 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+&qs614$44j65bll+n_s5h2rf5-dm0&h*uph)2rgub@tszi+vp'
